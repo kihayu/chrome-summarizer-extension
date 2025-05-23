a14600e0e9c6e4fccb4617a239cbb6d696240f61
Keanu Hie <hie.keanu@gmail.com>
2025-05-23 18:57:49 +0200

    chore: add ESLint and Prettier configuration with dependencies

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+
+// Create a basic config
+export default [
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/*.js']
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  prettierPlugin,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+        chrome: true,
+        document: true,
+        localStorage: true,
+        self: true,
+        window: true,
+        console: true,
+        process: true,
+        Bun: true,
+        SummarizerConstructor: true,
+        Summarizer: true,
+        TurndownService: true
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'no-case-declarations': 'off',
+      'no-undef': 'error'
+    }
+  }
+]
